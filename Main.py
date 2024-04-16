9eaccf467ebaf73494d026292afdb9fd1e199da3
JMR301 <janmarius.reeg@student.unisg.ch>
2024-04-17 00:07:15 +0200

    CSV datei wird abgerufen

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
-print("hello world")
+import streamlit as st
+import pandas as pd
+
+# Lade die CSV-Datei
+csv_datei_pfad = 'beispiel.csv'
+df = pd.read_csv(csv_datei_pfad)
+
+# Zeige die Daten in der Tabelle an
+st.write(df)
