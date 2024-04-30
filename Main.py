84d240c21ce6693265a39566d6db355dad424ba7
JMR301 <janmarius.reeg@student.unisg.ch>
2024-04-30 12:06:26 +0200

    Influencer.db change

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ df = pd.read_csv(csv_datei_pfad)
 # Step 2. Data Clean Up
 df.columns = df.columns.str.strip()
 # Step 3. Create/connect to a SQLite database
-connection = sqlite3.connect('demo.db')
+connection = sqlite3.connect('Influencer.db')
 # Step 4. Load data file to SQLite
 df. to_sql('Influencer Database',connection,if_exists='replace')
 # Step 5. close connection
