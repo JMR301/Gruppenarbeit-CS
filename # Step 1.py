6a6784bac49000e53340827171c00850670db806
JMR301 <janmarius.reeg@student.unisg.ch>
2024-04-30 11:16:04 +0200

    csv to sqlite

---
+++ # Step 1.py
@@ -0,0 +1,19 @@
+
+import streamlit as st 
+import pandas as pd
+import csv
+import sqlite3
+# Step 1. Laod data file
+csv_datei_pfad = 'top_insta_influencers_dataCSV.csv'
+df = pd. read_csv(csv_datei_pfad)
+
+
+# Step 2. Data Clean Up
+df.columns = df.columns.str.strip()
+# Step 3. Create/connect to a SQLite database
+connection = sqlite3. connect ('demo.db' )
+# Step 4. Load data file to SQLite
+# fail;replace; append
+df. to_sql('Influencer Database', connection, if_exists='replace')
+# Step 5. close connection
+connection.close()
--- Main.py
+++ Main.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import pandas as pd
+import csv
+import sqlite3
 
-# Lade die CSV-Datei
-csv_datei_pfad = 'beispiel.csv'
+# Step 1. Load data file
+csv_datei_pfad = '/Users/janreeg/HSG/Informatik/Gruppenarbeit CS/top_insta_influencers_dataCSV.csv'
 df = pd.read_csv(csv_datei_pfad)
 
-# Zeige die Daten in der Tabelle an
-st.write(df)
+
+# Step 2. Data Clean Up
+df.columns = df.columns.str.strip()
+# Step 3. Create/connect to a SQLite database
+connection = sqlite3.connect('demo.db')
+# Step 4. Load data file to SQLite
+df. to_sql('Influencer Database',connection,if_exists='replace')
+# Step 5. close connection
+connection.close()
