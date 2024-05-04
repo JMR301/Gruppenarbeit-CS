095f5f3d5e7bc68aef2be165b227bc0b436d96ad
JMR301 <janmarius.reeg@student.unisg.ch>
2024-05-04 15:11:57 +0200

    Filter wurden als pandas abfragen umgeschrieben

--- Filter.py
+++ Filter.py
@@ -1,51 +1,38 @@
+import pandas as pd
 import sqlite3
 
 class InfluencerFilter:
     def __init__(self, db_file):
         self.connection = sqlite3.connect(db_file)
-        self.cursor = self.connection.cursor()
+        self.load_all_data()
+
+    def load_all_data(self):
+        query = "SELECT * FROM 'Influencer Database'"
+        self.df = pd.read_sql_query(query, self.connection)
 
     def filter_by_category(self, category):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE Category=?", (category,))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[self.df['Category'] == category]
 
     def filter_by_range(self, column, min_val, max_val):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE {} BETWEEN ? AND ?".format(column), (min_val, max_val))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[(self.df[column] >= min_val) & (self.df[column] <= max_val)]
 
     def filter_by_posts(self, posts):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE posts=?", (posts,))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[self.df['posts'] == posts]
 
     def filter_by_followers(self, followers):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE followers=?", (followers,))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[self.df['followers'] == followers]
 
     def filter_by_avg_likes(self, avg_likes):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE avg_likes=?", (avg_likes,))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[self.df['avg_likes'] == avg_likes]
 
     def filter_by_60_day_eng_rate(self, rate):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE 60_day_eng_rate=?", (rate,))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[self.df['60_day_eng_rate'] == rate]
 
     def filter_by_new_post_avg_like(self, average_like):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE new_post_avg_like=?", (average_like,))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[self.df['new_post_avg_like'] == average_like]
 
     def filter_by_total_likes(self, total_likes):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE total_likes=?", (total_likes,))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[self.df['total_likes'] == total_likes]
 
     def filter_by_country(self, country):
-        self.cursor.execute("SELECT * FROM 'Influencer Database' WHERE country=?", (country,))
-        rows = self.cursor.fetchall()
-        return rows
+        return self.df[self.df['country'] == country]
--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import csv
 import sqlite3
-
+from Filter import InfluencerFilter
 # Step 1. Load data file
 csv_datei_pfad = '/Users/janreeg/HSG/Informatik/Gruppenarbeit CS/top_insta_influencers_dataCSV.csv'
 df = pd.read_csv(csv_datei_pfad, sep=';')
@@ -13,7 +13,10 @@ df.columns = df.columns.str.strip()
 # Step 3. Create/connect to a SQLite database
 connection = sqlite3.connect('Influencer.db')
 # Step 4. Load data file to SQLite
-df. to_sql('Influencer Database',connection,if_exists='replace')
+df.to_sql('Influencer Database',connection,if_exists='replace')
+
+
+
 
 query1 = "SELECT * FROM 'Influencer Database'"
 
@@ -36,6 +39,8 @@ query4 = "PRAGMA table_info(Influencer Database)"
 query5 = "SELECT name FROM sqlite_master WHERE type='table';"
 
 Tablename = pd.read_sql(query5, connection)
+
+
 
 # Step 5. close connection
 connection.close()
